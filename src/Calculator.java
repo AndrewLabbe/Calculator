848fe0427c2bcf0942a716d9c0ef8fd8ec7cb339
AndrewLabbe <ablabbe@quinnipiac.edu>
2022-02-07 18:29:49 -0500

    adds, subtracts, multiplies or divides two numbers which can now handle decimals

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,10 +5,10 @@ public class Calculator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Please enter two integer whole numbers.");
+		System.out.println("Please enter two numbers.");
 		Scanner scanner = new Scanner(System.in);
-		int num1 = scanner.nextInt();
-		int num2 = scanner.nextInt();
+		double num1 = scanner.nextInt();
+		double num2 = scanner.nextInt();
 		System.out.println("Would you like to add, subtract, multiply or divide the two numbers?");
 		Scanner scanner2 = new Scanner(System.in);
 		String res = scanner2.nextLine();
