d646127315436ee891c9de4c004e7b55d4aa8828
AndrewLabbe <ablabbe@quinnipiac.edu>
2022-02-07 18:05:07 -0500

    adds two numbers

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Calculator {
-
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Please enter two integer whole numbers.");
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		System.out.println(num1 + num2);
+	}
 }
